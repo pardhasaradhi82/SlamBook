54006cb2516b6d1a8f0247480b9fac44ebc51bd5
Pardhu raam <128017734+pardhasaradhi82@users.noreply.github.com>
2023-06-21 04:35:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ const slamSchema = mongoose.Schema({
   const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URI);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.log(error);
       process.exit(1);
